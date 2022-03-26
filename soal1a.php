3fd599a2ec8fcb2b68e6824036287ceaf92e4254
budprast <prasetiobudi72Gmail.com>
2022-03-26 17:09:41 +0700

    menambahkan file soal1a.php

---
+++ soal1a.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    </style>
+</head>
+<body>
+    <form>
+        <fieldset style="width:0px">
+            <label for="baris">Inputkan Jumlah Baris:</label>
+            <input type="text" name="baris" id="baris"><br><br>
+            <label for="kolom">Inputkan Jumlah Kolom:</label>
+            <input type="text" name="kolom" id="kolom"> <br><br>
+            <button type="submit" name="submit"  value="submit">SUBMIT</button>
+        </fieldset>
+    </form>   
+</body>
+</html>
